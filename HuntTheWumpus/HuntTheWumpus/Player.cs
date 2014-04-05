83b2c4e421037b4470e54c9a825042ff33f7a817
scythetwirler <pi4fish@gmail.com>
2014-04-04 22:30:11 -0700

    Fix #8. Adds comments to Player.cs.

--- HuntTheWumpus/HuntTheWumpus/Player.cs
+++ HuntTheWumpus/HuntTheWumpus/Player.cs
@@ -7,17 +7,19 @@ namespace HuntTheWumpus
 {
     class Player
     {
-        private const int ARROW_PURCHASE = 2, SCORE_CONSTANT = 100,
+        private const int ARROW_PURCHASE = 2, SCORE_WIN_CONSTANT = 100,
             ARROW_MULTIPLIER = 10, DEFAULT_ARROW_COUNT = 3, DEFAULT_TURN_COUNT = 0,
             DEFAULT_COIN_COUNT = 10;
         private int arrows, goldCoins, turns;
 
         public Player() 
         {
+            // initialize the defaults for a new player
             arrows = ARROW_PURCHASE;
             goldCoins = DEFAULT_COIN_COUNT;
             turns = DEFAULT_TURN_COUNT;
         }
+
         public Player(int arrows, int goldCoins, int turns)
         {
             this.arrows = arrows;
@@ -43,29 +45,34 @@ namespace HuntTheWumpus
             set { turns = value; }
         }
 
+        // Successful purchase through trivia questions
         public void purchaseArrowsSuccess()
         {
             arrows += ARROW_PURCHASE;
         }
 
+        // Increment gold after moving tunnels
         public void moveGoldIncrement()
         {
             goldCoins++;
         }
 
+        // Each time a question is asked, a coin is subtracted
         public void triviaQuestionAsked()
         {
             goldCoins--;
         }
 
+        // The game progresses; increment the turn
         public void incrementTurn()
         {
             turns++;
         }
 
+        // Returns the player's final score after killing the Wumpus
         public int computeScore()
         {
-            return SCORE_CONSTANT - turns + goldCoins + (ARROW_MULTIPLIER * arrows);
+            return SCORE_WIN_CONSTANT - turns + goldCoins + (ARROW_MULTIPLIER * arrows);
         }
     }
 }
